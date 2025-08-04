963a2bd718cf7cca7239b649cb9238ed2dcf156b
princebaye223 <herokubotndao@gmail.com>
2025-08-04 11:56:22 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  SESSION_ID: "malvin~lv1SnLDC#x7FdsrW5DXLYkbMrqILBdbw0IFDj01rZedo_iJxa1Yo",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "768125592", // PUT OWNER NUMBER HERE
+  PREFIX: "#" // prefix (e.g., ., /, !, *)
 };
